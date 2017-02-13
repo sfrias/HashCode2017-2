178968cd0010f53f1ccf0dfaae5ef6b206840e2c
Shachar Langbeheim <shachar@lightricks.com>
2017-02-13 19:20:53 +0200

    Temp

--- Pizza/PizzaParser.py
+++ Pizza/PizzaParser.py
@@ -5,6 +5,40 @@ def readFile(fileName):
     firstLine = file.readline().replace('\n','')
     splitFirstLine = firstLine.split(' ')
     pizza = file.read().split('\n')
-    solver.solve(splitFirstLine[0], splitFirstLine[1], splitFirstLine[2], splitFirstLine[3], pizza)
+    solution = solver.solve(splitFirstLine[0], splitFirstLine[1], splitFirstLine[2], splitFirstLine[3], pizza)
+    if (verify(pizza, solution, splitFirstLine[2], splitFirstLine[3])):
+        print(evaluateSolution(pizza, solution))
+    print('we failed :(')
+
+def verify(pizza, solution, minimum, maximum):
+    if (verifyOverlap(solution) == False):
+        return False
+
+    for rect in solution:
+        if (verifyRectangle(rect, pizza, minimum, maximum) == False):
+            return False
+
+    return True
+
+def verifyOverlap(solution):
+    index = 0
+    secondaryIndex = 1
+    while index < len(solution):
+        while secondaryIndex < len(solution):
+            rect1 = solution[index]
+            rect2 = solution[secondaryIndex]
+            if rectsOverlap(rect1, rect2):
+                return False
+
+    return True
+
+def rectsOverlap(rect1, rect2):
+    return (rect1.x1 > rect2.x2 | rect2.x1 > rect1.x2) & (rect1.y1 > rect2.y2 | rect2.y1 > rect1.y2)
+
+def verifyRectangle(rect, pizza, minimum, maximum):
+    print('give me a moment')
+
+def evaluateSolution(pizza, solution):
+    print('it\'s ok!')
 
 readFile('pizzas/small.in')
